bfd0fb7423dba24812f368f3fb1b05e45cd259d8
joeshawky <102739596+joeshawky@users.noreply.github.com>
2026-03-08 17:09:56 +0300

    updated import statements

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 import Footer from "./components/Footer/Footer";
-import SkillsSection from "./components/SkillsSection/SkillsSection";
-import ProjectsSection from "./components/ProjectsSection/ProjectsSection";
+import SkillsSection from "./components/SkillsSection";
+import ProjectsSection from "./components/ProjectsSection";
 
 gsap.registerPlugin(useGSAP, MotionPathPlugin); // register the hook to avoid React version discrepancies
 
